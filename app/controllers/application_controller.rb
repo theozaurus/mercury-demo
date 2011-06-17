5aca0b4a1876a89d922512103be3e03f81c9ac37
Jeremy Jackson <jejacks0n@gmail.com>
2011-06-17 13:52:03 -0600

    added basic demo app -- minus mercury for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # stub for content pages
+  def show
+    render action: "#{params[:content_page] || 'intro'}"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 MercuryDemo::Application.routes.draw do
+
+  root to: "application#show"
+  match '/content(/*content_page)' => "application#show"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
